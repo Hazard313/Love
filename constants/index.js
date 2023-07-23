236a16d24c55d7584fe6731d0c533f9bc062b3be
Hazard313 <106491697+Hazard313@users.noreply.github.com>
2023-07-23 12:19:23 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xf36fb5c3ced97469ca9413c8e5e0a90d68f01c45";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
